cd2244fde2ea6d23be8ee3e5c3f9b950af2d70b5
Antonio Voce <antonio.voce@guardian.co.uk>
2018-07-17 17:21:57 +0100

    Set up proj, add us topojson

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,24 @@
-// javascript goes here
+import * as d3 from 'd3';
+import * as topojson from 'topojson'
+import us from '../assets/us-map.json'
+
+const mapWidth = 1200;
+const mapHeight = 800;
+
+const svg = d3.select('.us-map').append('svg')
+  .attr('width', mapWidth)
+  .attr('height', mapHeight)
+
+const path = d3.geoPath();
+
+  svg.append("path")
+    .attr("stroke", "#aaa")
+    .attr("stroke-width", 0.5)
+    .attr("d", path(topojson.mesh(us, us.objects.counties, function (a, b) { return a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0); })));
+
+  svg.append("path")
+    .attr("stroke-width", 0.5)
+    .attr("d", path(topojson.mesh(us, us.objects.states, function (a, b) { return a !== b; })));
+
+  svg.append("path")
+    .attr("d", path(topojson.feature(us, us.objects.nation)));
--- src/render.js
+++ src/render.js
@@ -1,4 +1,13 @@
 import templateHTML from "./src/templates/main.html!text"
+import fs from 'fs'
+import csvParse from 'csv-parse/lib/sync';
+// import usData from '../assets/us-climate.csv';
+
+const csv = fs.readFileSync('src/assets/us-climate.csv', 'utf8')
+
+const parsed = csvParse(csv, { columns: true })
+
+
 
 export async function render() {
     // this function just has to return a string of HTML
