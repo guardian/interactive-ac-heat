c0f780adae372ff1685e69a1675a4d052118cf9f
Antonio Voce <antonio.voce@guardian.co.uk>
2018-07-18 16:05:45 +0100

    Path changes

--- src/js/app.js
+++ src/js/app.js
@@ -11,14 +11,29 @@ const svg = d3.select('.us-map').append('svg')
 
 const path = d3.geoPath();
 
-  svg.append("path")
-    .attr("stroke", "#aaa")
-    .attr("stroke-width", 0.5)
-    .attr("d", path(topojson.mesh(us, us.objects.counties, function (a, b) { return a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0); })));
+  // svg.append("path")
+  //   .attr("stroke", "#aaa")
+  //   .attr("stroke-width", 0.5)
+  //   .attr('fill', 'none')
+  //   .attr("d", path(topojson.mesh(us, us.objects.counties, function (a, b) { return a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0); })));
 
-  svg.append("path")
-    .attr("stroke-width", 0.5)
-    .attr("d", path(topojson.mesh(us, us.objects.states, function (a, b) { return a !== b; })));
+  // svg.append("path")
+  //   .attr("stroke-width", 0.5)
+  //   .attr('stroke', 'black')
+  //   // .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)))
+  //   .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)))
+  //   .style('fill', 'yellow')
 
-  svg.append("path")
-    .attr("d", path(topojson.feature(us, us.objects.nation)));
+  svg.selectAll('state')
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter()
+    .append('path')
+    .attr('d', path)
+    .style('fill', d => d.id === '06' ? 'yellow' : 'grey')
+    .style('stroke', 'black')
+    .attr("stroke-width", 0.5)
+
+  // svg.append("path")
+  //   .attr("d", path(topojson.feature(us, us.objects.nation)))
+  //   .attr('fill', 'none')
+  //   .attr('stroke', 'black')
